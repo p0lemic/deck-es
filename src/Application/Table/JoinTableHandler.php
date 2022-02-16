6c86ab3830102522c994a9a221d6e4563ee6071a
Javier Roman <javierroman@drinksandco.com>
2022-02-16 12:29:49 +0100

    Test JoinTableHandler

--- src/Application/Table/JoinTableHandler.php
+++ src/Application/Table/JoinTableHandler.php
@@ -2,10 +2,11 @@
 
 namespace Deck\Application\Table;
 
+use Broadway\CommandHandling\CommandHandler;
 use Deck\Domain\Table\TableRepositoryInterface;
 use Deck\Domain\User\PlayerReadModelRepositoryInterface;
 
-class JoinTableHandler
+class JoinTableHandler implements CommandHandler
 {
     private TableRepositoryInterface $tableRepository;
     private PlayerReadModelRepositoryInterface $playerReadModelRepository;
@@ -18,7 +19,7 @@ class JoinTableHandler
         $this->playerReadModelRepository = $playerReadModelRepository;
     }
 
-    public function handle(JoinTableCommand $joinTableCommand): void
+    public function handle($joinTableCommand): void
     {
         $player = $this->playerReadModelRepository->findByIdOrFail($joinTableCommand->playerId());
         $table = $this->tableRepository->get($joinTableCommand->tableId());
